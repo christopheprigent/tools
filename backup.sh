ca317b259a74140a6e71306a868053e146859c51
ad0726 <ad0726@hotmail.fr>
2019-03-02 01:22:03 +0100

    Add var to env and translate log

--- backup.sh
+++ backup.sh
@@ -1,42 +1,37 @@
 #!/bin/sh
 
-# Date du jour
+# Date of the day
 DATE=`date +%Y-%m-%d`
-# Dossier à sauvergarder (dossier dans lequel les sites sont placés)
-DIRTOBCK="/var/www/html/owncloud/DrWhat/files/Marvel"
-# Dossier de sauvegarde temporaire des sites
-DIRBCK="/var/archives"
-# Nom du fichier
+# File name
 NAME="backup-$DATE.tar.gz"
-# Destination
-DIRUPLOAD="/owncloud/DrWhat"
 # Log file for current script
 DIRLOG="./log"
 LOG="${DIRLOG}/${DATE}_log.log"
 # Sensitive vars : FTP_SERVER FTP_LOGIN and FTP_PASS
 source .env
 
-echo "$(date +%F_%T) : == Starting $0 =="               >> $LOG
-echo "my Process ID is `$$`"                            >> $LOG
-echo "Création du répertoire de sauvegarde"             >> $LOG
+echo "$(date +%F_%T) : == Starting $0 =="                 >> $LOG
+echo "My Process ID is `$$`"                              >> $LOG
+
 if [ ! -d $DIRBCK ]; then
+  echo "Establishment of the backup directory"            >> $LOG
   mkdir -p $DIRBCK
 fi
-echo "Création du répertoire de log"                    >> $LOG
 if [ ! -d $DIRLOG ]; then
+  echo "Establishment of the log directory"               >> $LOG
   mkdir -p $DIRLOG
 fi
-echo "Création de l'archive des sites"                  >> $LOG
+echo "Establishment of the backup"                        >> $LOG
 tar -cvzf backup-$DATE_FORMAT.tar.gz $DIRTOBCK > $DIRBCK
 
-echo "Connexion au serveur FTP et envoi des données"    >> $LOG
+echo "Connect to FTP server and send data"                >> $LOG
 ftp -n $FTP_SERVER <<END
         user $FTP_LOGIN $FTP_PASS
         put $NAME $DIRUPLOAD/$NAME
         quit
 END
 
-echo "Suppression de l'archive de sauvegarde"           >> $LOG
+echo "Remove local backup"                                >> $LOG
 rm $DIRBCK/$NAME
 
-echo "$(date +%F_%T) : == End =="                       >> $LOG
+echo "$(date +%F_%T) : == End =="                         >> $LOG
