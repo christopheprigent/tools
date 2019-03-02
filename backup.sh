2a94fe2b924bd70b5f5a105ec51d959f02eb58e4
MR CHRISTOPHE PRIGENT <christophe.prigent@gmail.com>
2019-03-02 10:21:01 +0100

    fix backup file name
    use fullpath backup filename for tar  and ftp put
    ftp put with directorie as dest will keep the original filename

--- backup.sh
+++ backup.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
+# todo : maybe add `cd $ROOT` at the begining of this script to secure cron tab execution
+# cron are run with pwd = /root or user ~
+# ROOT="/FULLPATH/OF/THIS/SCRIPT/DIRECTORY"
+# cd $ROOT
 
 # Date of the day
-DATE=`date +%Y-%m-%d`
-# File name
-NAME="backup-$DATE.tar.gz"
-# Log file for current script
+TODAY=`date +%Y-%m-%d`
+# Folder to save (sites are here)
+# todo : maybe move to .env ?
+DIRTOBCK="/var/www/html/owncloud/DrWhat/files/Marvel"
+# backups destination folder
+DIRBCK="/var/archives"
+# logs folder
 DIRLOG="./log"
-LOG="${DIRLOG}/${DATE}_log.log"
+# Nom du fichier de sauvegarde
+BCK_NAME="backup-$TODAY.tar.gz"
+# Chemin complet
+BCK="${DIRBCK}/${BCK_NAME}"
+# Log file for current script
+LOG="${DIRLOG}/${TODAY}_log.log"
 # Sensitive vars : FTP_SERVER FTP_LOGIN and FTP_PASS
 source .env
 
@@ -22,16 +34,16 @@ if [ ! -d $DIRLOG ]; then
   mkdir -p $DIRLOG
 fi
 echo "Establishment of the backup"                        >> $LOG
-tar -cvzf backup-$DATE_FORMAT.tar.gz $DIRTOBCK > $DIRBCK
+tar -cvzf $BCK $DIRTOBCK
 
 echo "Connect to FTP server and send data"                >> $LOG
 ftp -n $FTP_SERVER <<END
         user $FTP_LOGIN $FTP_PASS
-        put $NAME $DIRUPLOAD/$NAME
+        put $BCK $DIRUPLOAD/
         quit
 END
 
 echo "Remove local backup"                                >> $LOG
-rm $DIRBCK/$NAME
+rm $BCK
 
 echo "$(date +%F_%T) : == End =="                         >> $LOG
